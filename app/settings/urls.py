2f623ed81f5ea9daa28281adfec248ad12f91b52
Dmitry Yaroshevsky <89291924+dimonyar@users.noreply.github.com>
2022-02-19 14:58:42 +0200

    fix Import statements in wrong order

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,6 +1,8 @@
+from currency import views as currency_views
+
 from django.contrib import admin
 from django.urls import path
-from currency import views as currency_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
