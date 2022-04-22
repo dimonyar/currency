6c1086bfb42b67e741cbe39a1a553a94cfad54dd
Dmitry Yaroshevsky <89291924+dimonyar@users.noreply.github.com>
2022-04-22 14:09:14 +0300

    add Source.code_name and Rate.base_type

--- app/currency/model_choices.py
+++ app/currency/model_choices.py
@@ -4,3 +4,12 @@ from django.db import models
 class RateType(models.TextChoices):
     USD = 'USD', 'Dollar'
     EUR = 'EUR', 'Euro'
+    BTC = 'BTC', 'Bitcoin'
+    UAH = 'UAH', 'Hryvnia'
+
+
+class SourceCodeName(models.IntegerChoices):
+    PRIVATBANK = 1, 'PrivatBank'
+    MONOBANK = 2, 'MonoBank'
+    VKURSE = 3, 'vkurse.dp.ua'
+
