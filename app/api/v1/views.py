e27869ed81fc34bf1e95b7bcd2e83d97dc2de12f
Dmitry Yaroshevsky <89291924+dimonyar@users.noreply.github.com>
2022-05-11 17:31:32 +0300

    SourceFilter and ContactusFilter

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,7 +1,10 @@
+from api.v1.filters import ContactusFilter, SourceFilter
 from api.v1.pagination import RatesPagination
 from api.v1.serializers import ContactusSerializer, RateSerializer, SourceSerializer
 
 from currency.models import ContactUs, Rate, Source
+
+from django_filters import rest_framework as filters
 
 from rest_framework import generics, viewsets
 from rest_framework.renderers import JSONRenderer
@@ -14,12 +17,20 @@ from rest_framework_xml.renderers import XMLRenderer
 class SourceView(generics.ListAPIView):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
+    filterset_class = SourceFilter
+    filter_backends = (
+        filters.DjangoFilterBackend,
+    )
 
 
 class ContactusViewSet(viewsets.ModelViewSet):
     queryset = ContactUs.objects.all()
     serializer_class = ContactusSerializer
     renderer_classes = (JSONRenderer, XMLRenderer, CSVRenderer)
+    filterset_class = ContactusFilter
+    filter_backends = (
+        filters.DjangoFilterBackend,
+    )
 
 
 class RateViewSet(viewsets.ModelViewSet):
