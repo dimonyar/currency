f7107c62fe944f1214a981eec92d201612ebbfd5
Dmitry Yaroshevsky <89291924+dimonyar@users.noreply.github.com>
2022-06-09 16:55:50 +0300

    ContactUs with permission

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,4 +1,4 @@
-from currency.models import Source
+from currency.models import ContactUs, Source
 
 from django.contrib import admin
 
@@ -11,4 +11,22 @@ class SourceAdmin(admin.ModelAdmin):
     )
 
 
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        'email_from',
+        'subject',
+        'message',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(Source, SourceAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
